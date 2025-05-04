00d943c08df2284645feee803cec3a093f9f10c1
lmartinez1701 <lucas.carvalho1945@gmail.com>
2025-05-03 22:17:29 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -7,6 +7,10 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
 app.use(cors());
 app.use(express.json());
 
